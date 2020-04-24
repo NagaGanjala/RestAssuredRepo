4618dd8391e1d278a8d51e0e78604d4aa66ab56b
NagaGanjala <Naga.Ganjala@gmail.com>
2020-04-24 11:17:00 +0530

    RestAssured Sample Examples

---
+++ Rest_Assured_Udemy/twitterexample/src/test/java/twitterexample/TwitterTest.java
@@ -0,0 +1,56 @@
+package twitterexample;
+
+
+import static io.restassured.RestAssured.*;
+
+import org.junit.BeforeClass;
+import org.junit.Ignore;
+import org.junit.Test;
+
+import io.restassured.RestAssured;
+
+public class TwitterTest {
+	
+	String consumerKey = "YOUR_CLIENTKEY";
+	String consumerSecret = "YOUR_CLIENT_SECRET";
+	String accessTokenSecret = "YOUR_ACCESSKEY";
+	String secretToken = "YOUR_ACCESSSECRET";
+	
+	@BeforeClass
+	public static void init(){
+		RestAssured.baseURI="https://api.twitter.com";
+		RestAssured.basePath="/1.1/statuses";
+	}
+	
+	@Ignore
+	@Test
+	public void createTweet(){
+		given()
+		.auth()
+		.oauth(consumerKey, consumerSecret, accessTokenSecret, secretToken)
+		.queryParam("status","This is my first tweet from Rest Assured")
+		.log()
+		.all()
+		.when()
+		.post("/update.json")
+		.then()
+		.log()
+		.all();
+	}
+	
+	@Test
+	public void getTweets(){
+		given()
+		.auth()
+		.oauth(consumerKey, consumerSecret, accessTokenSecret, secretToken)
+		.queryParam("screen_name","YOUR SCREEN NAME")
+		.log()
+		.all()
+		.when()
+		.get("/user_timeline.json")
+		.then()
+		.log()
+		.all();
+	}
+
+}
